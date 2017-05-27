3017d8bed6c425847d0657123e3a77cba8f121ea
michaelwan <michaelwan0906@hotmail.com>
2017-05-27 10:08:48 +0800

    optimize

--- interface/app.js
+++ interface/app.js
@@ -6,18 +6,27 @@ var app        = require('express')()
 var server     = require('http').Server(app)
 var io         = require('socket.io')(server)
 var http       = require('http')
-var bingo      = require('./static/js/bingo_server.js')
+var colors     = require('colors')
 var config     = require("./config.json")
-var DEBUG = true
 
+// set up app
 var port = config.port
+console.log(("Server is running at port " + port).magenta)
+server.listen(port)
 
+// global variable
+var DEBUG = config.DEBUG
+
+// including bingo.js
+var bingo = require('./static/js/bingo_server.js')
+var print = bingo.print 
 bingo.init({ 
 	'io':io,
 	'config':config,
 	'DEBUG':DEBUG
 });
 
+// middlewares
 app.use('/assets', express.static(__dirname + '/assets'))
 app.use('/static', express.static(__dirname + '/static'))
 app.get('/', function(req, res){
@@ -36,11 +45,12 @@ app.get('/spawn', function(req, res){
 	})
 })
 
+// socket.io part
 io.sockets.on('connection', function(socket){
 	
 	// initialize
 	let player = new bingo.Player(socket)
-	console.log("[Bingo] Someone connented.")
+	print("[Bingo] Someone connented.")
 	socket.emit('refreshRoomInfo', { 'list':bingo.getSimpleRoomList() })
 
 	// socket events
@@ -76,11 +86,11 @@ io.sockets.on('connection', function(socket){
         if (!player.rid) return    
         let room = bingo.getRoomByRid(player.rid)
         if(room.playerList[0]!=player){
-            console.log('Permission denied')
+            print('Permission denied')
             return
         }
         room.AIConfig = data.config
-        if(DEBUG) console.log("[Debug] Config changed:", room.AIConfig)
+        if(DEBUG) print("[Debug] Config changed:", room.AIConfig)
     })
 
 	// for admin
@@ -101,11 +111,12 @@ io.sockets.on('connection', function(socket){
         }
     })
 	socket.on('check', function(params){
-		if (params == 'roomList') console.log(bingo.getSimpleRoomList())
-		if (params == 'id') console.log(player.id)
+		if (params == 'roomList') print(bingo.getSimpleRoomList())
+		if (params == 'id') print(player.id)
 	})
 })
 
+// functions for Req process
 function joinRoom(player, rid){
 	player.joinRoom(bingo.getRoomByRid(rid))
 	player.socket.emit('message', {'message':'Join room successfully.', 'msgId':bingo.randomString(8)})
@@ -129,7 +140,7 @@ function createRoom(player, data){
 	player.joinRoom(newRoom);
 	player.socket.emit('createRoomRes', {'status':'success','rid':newRoom.rid})
 	io.emit('refreshRoomInfo', { 'list':bingo.getSimpleRoomList() })
-	if (DEBUG) console.log("[Bingo] Player", player.name, "created room", data.roomName)
+	print("[Bingo] Player", player.name, "created room", data.roomName)
 }
 
 function searchPlayerInRoomList(socket){
@@ -143,18 +154,3 @@ function searchPlayerInRoomList(socket){
 function escape(str){
 	return str.replace(/&/g, "&amp").replace(/</g, "&lt").replace(/>/g, "&gt")
 }
-
-function jizz(){
-	for (let i=0;i<5;i++){
-		let a = Math.floor(Math.random()*10+1)
-		let b = Math.floor(Math.random()*10+1)
-		console.log(Math.min(a, b), Math.max(a, b), Math.floor(Math.random()*50))
-	}
-	for (var i = 0; i < 8; i++) {
-		console.log(Math.floor(Math.random()*10+1),Math.floor(Math.random()*20)+1)
-	};
-}
-
-server.listen(port, function(){
-	console.log("Server is running at port", port)
-})
